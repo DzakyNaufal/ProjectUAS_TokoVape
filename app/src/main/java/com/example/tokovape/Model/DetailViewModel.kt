c5609429761fe119901b19a44d50966d0c39a583
DzakyNaufal <dzakynmulna@gmail.com>
2024-01-12 05:39:49 +0700

    membuat class DetailViewModel by Dzaky Naufal Maulana (20210140033)

--- app/src/main/java/com/example/tokovape/Model/DetailViewModel.kt
+++ app/src/main/java/com/example/tokovape/Model/DetailViewModel.kt
@@ -11,6 +11,26 @@ import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
 
+class DetailViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoryToko: RepositoriToko
+): ViewModel(){
+    private val tokoId: Int = checkNotNull(savedStateHandle[DetailDestination.tokoIdArg])
+    val uiState: StateFlow<ItemDetailsUIState> =
+        repositoryToko.getTokoStream(tokoId)
+            .filterNotNull()
+            .map {
+                ItemDetailsUIState(detailToko = it.toDetailToko())
+            }.stateIn(
+                scope = viewModelScope,
+                started = SharingStarted.WhileSubscribed(TIME_MILLIS),
+                initialValue = ItemDetailsUIState()
+            )
+    
+    companion object{
+        private const val TIME_MILLIS = 5_000L
+    }
+}
 
 data class ItemDetailsUIState(
     val outOfStock: Boolean = true,
