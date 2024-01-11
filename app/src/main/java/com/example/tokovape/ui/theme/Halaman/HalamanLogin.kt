32544986b52f623c871fc7d0ed0fa52c9b8b3f97
DzakyNaufal <dzakynmulna@gmail.com>
2024-01-12 06:05:17 +0700

    membuat object DestinasiLogin by Dzaky Naufal Maulana (20210140033)

---
+++ app/src/main/java/com/example/tokovape/ui/theme/Halaman/HalamanLogin.kt
@@ -0,0 +1,40 @@
+package com.example.tokovape.ui.theme.Halaman
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedCard
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.tokovape.R
+import com.example.tokovape.navigasi.DestinasiNavigasi
+import com.example.tokovape.ui.theme.TokoVapeTheme
+
+object DestinasiLogin : DestinasiNavigasi {
+    override val route = "login"
+    override val titleRes = R.string.login
+}
+
+
