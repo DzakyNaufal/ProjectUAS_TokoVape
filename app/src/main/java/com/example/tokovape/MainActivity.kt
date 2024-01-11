f44d201f16dd934cbbf17c49b1b3bbe095e621fa
DzakyNaufal <dzakynmulna@gmail.com>
2024-01-12 05:59:15 +0700

    membuat object DetailDestination by Dzaky Naufal Maulana (20210140033)

--- app/src/main/java/com/example/tokovape/MainActivity.kt
+++ app/src/main/java/com/example/tokovape/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.tokovape.navigasi.TokoApp
 import com.example.tokovape.ui.theme.TokoVapeTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +23,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    TokoApp()
                 }
             }
         }
