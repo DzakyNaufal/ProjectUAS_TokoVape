8186a5321b84bd7ab8e5d62e639ce83159c2505a
DzakyNaufal <dzakynmulna@gmail.com>
2024-01-12 05:35:27 +0700

    membuat class EntryViewModel by Dzaky Naufal Maulana (20210140033)

---
+++ app/src/main/java/com/example/tokovape/Model/EntryViewModel.kt
@@ -0,0 +1,36 @@
+package com.example.tokovape.Model
+
+import android.app.AlarmManager
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.tokovape.DataToko.Toko
+import com.example.tokovape.Repository.RepositoriToko
+import java.util.NavigableMap
+
+class EntryViewModel(private val repositoriToko: RepositoriToko): ViewModel() {
+    /*
+    * Berisi status toko saat ini
+    */
+    var  uiStateToko by mutableStateOf(UIStateToko())
+        private set
+
+    /* Fungsi untuk menvalidasi imput */
+    private fun validasiInput (uiState: DetailToko = uiStateToko.detailToko) : Boolean{
+        return  with(uiState) {
+            nama.isNotBlank() && harga.isNotBlank() && total.isNotBlank()
+        }
+    }
+    fun updateUiState(detailToko: DetailToko) {
+        uiStateToko =
+            UIStateToko(detailToko = detailToko, isEntryValid = validasiInput(detailToko))
+    }
+
+    /*Fungsi Untuk menyimpan data yang di-entry */
+    suspend fun saveToko(){
+        if (validasiInput()) {
+            repositoriToko.insertToko(uiStateToko.detailToko.toToko())
+        }
+    }
+}
