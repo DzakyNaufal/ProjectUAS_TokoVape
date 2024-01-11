246b727ede7b035579a1769292c8427fd7e27e3d
DzakyNaufal <dzakynmulna@gmail.com>
2024-01-12 05:42:22 +0700

    membuat class EditViewModel by Muhammad Nabil Makarim (20210140009)

---
+++ app/src/main/java/com/example/tokovape/Model/EditViewModel.kt
@@ -0,0 +1,33 @@
+package com.example.tokovape.Model
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.tokovape.Repository.RepositoriToko
+import com.example.tokovape.ui.theme.Halaman.ItemEditDestination
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
+
+class EditViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoriToko: RepositoriToko
+) : ViewModel() {
+
+    var tokoUiState by mutableStateOf(UIStateToko())
+        private set
+
+    private val itemId: Int = checkNotNull(savedStateHandle[ItemEditDestination.itemIdArg])
+
+    init {
+        viewModelScope.launch {
+            tokoUiState = repositoriToko.getTokoStream(itemId)
+                .filterNotNull()
+                .first()
+                .toUiStateToko(true)
+        }
+    }
+}
