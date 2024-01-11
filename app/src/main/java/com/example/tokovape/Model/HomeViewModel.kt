3f6a89689d1bc3855d7ddc206569a42206959434
DzakyNaufal <dzakynmulna@gmail.com>
2024-01-12 05:47:00 +0700

    membuat class HomeViewModel by Dzaky Naufal Maulana (20210140033)

---
+++ app/src/main/java/com/example/tokovape/Model/HomeViewModel.kt
@@ -0,0 +1,28 @@
+package com.example.tokovape.Model
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.tokovape.DataToko.Toko
+import com.example.tokovape.Repository.RepositoriToko
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+
+
+class HomeViewModel(private val RepositoriToko: RepositoriToko): ViewModel() {
+
+    companion object {
+        private const val TIMEOUT_MILLIS = 5_000L
+    }
+
+    val homeUiState: StateFlow<HomeUiState> = RepositoriToko.getAllTokoStream().filterNotNull()
+
+        .map { HomeUiState(listToko = it.toList()) }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(TIMEOUT_MILLIS),
+            initialValue = HomeUiState()
+        )
+}
