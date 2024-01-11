2edc89a60a7c1e5e4f6a7062d949920ee98a92a4
DzakyNaufal <dzakynmulna@gmail.com>
2024-01-12 05:48:23 +0700

    membuat fun CreationExtras.aplikasiToko by Dzaky Naufal Maulana (20210140033)

---
+++ app/src/main/java/com/example/tokovape/Model/PenyediaViewModel.kt
@@ -0,0 +1,17 @@
+package com.example.tokovape.Model
+
+import android.text.Spannable.Factory
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.tokovape.AplikasiToko
+
+
+/**
+ * Fungsi ekstensi query untuk objek [Application] dan mengembalikan sebuah instance dari
+ *  [AplikasiToko]
+ */
+fun CreationExtras.aplikasiToko(): AplikasiToko =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiToko)
