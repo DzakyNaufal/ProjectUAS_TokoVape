8ad5841dbcad70e17d646d70e0a28f83536c773f
DzakyNaufal <dzakynmulna@gmail.com>
2024-01-12 05:28:23 +0700

    membuat file toko dao dan interface TokoDao dan menambahkan fun insert, getToko,getAllToko by Dzaky Naufal Maulana (20210140033)

---
+++ app/src/main/java/com/example/tokovape/DataToko/TokoDao.kt
@@ -0,0 +1,21 @@
+package com.example.tokovape.DataToko
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface TokoDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert (toko: Toko)
+
+    @Query("SELECT * from tblToko WHERE id = :id")
+    fun getToko(id: Int): Flow<Toko>
+
+    @Query ("SELECT * FROM tblToko ORDER BY nama ASC")
+    fun getAllToko(): Flow<List<Toko>>
+}
