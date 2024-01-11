cf84ae18f8191774103744df0b094b28a252fac6
DzakyNaufal <dzakynmulna@gmail.com>
2024-01-12 05:24:34 +0700

    membuat abstract fun TokoDao dan companion object commit by Muhammad Nabil Makarim (20210140009)

--- app/src/main/java/com/example/tokovape/DataToko/DataBaseToko.kt
+++ app/src/main/java/com/example/tokovape/DataToko/DataBaseToko.kt
@@ -8,4 +8,19 @@ import androidx.room.RoomDatabase
 @Database(entities = [Toko:: class], version = 1, exportSchema = false)
 abstract class DatabaseToko : RoomDatabase(){
 
+    abstract fun TokoDao() : TokoDao
+
+    companion object{
+        @Volatile
+        private var Instance: DatabaseToko? = null
+
+        fun getDatabase(context: Context): DatabaseToko{
+            return (Instance?: synchronized(this    ){
+                Room.databaseBuilder(context,
+                    DatabaseToko::class.java,
+                    "toko_database")
+                    .build().also { Instance=it }
+            })
+        }
+    }
 }
